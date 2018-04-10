4a8dcac0ee41b4fc8c465f93fe0926d7e12be3dc
damian.przygodzki <damian.przygodzki@jazzy.pro>
2018-04-10 10:45:51 +0200

    #140 Manifest as build parameter (change request #2)

--- packages/utils/skpm-config.js
+++ packages/utils/skpm-config.js
@@ -4,7 +4,7 @@ module.exports = function getSkpmConfig(packageJSON, argv) {
   const skpmConfig = packageJSON.skpm || {}
   return {
     main: skpmConfig.main || packageJSON.main,
-    manifest: argv.manifest || skpmConfig.manifest || packageJSON.manifest,
+    manifest: typeof argv !== "undefined" && argv.manifest || skpmConfig.manifest || packageJSON.manifest,
     version: skpmConfig.version || packageJSON.version,
     homepage: skpmConfig.homepage || packageJSON.homepage,
     description: skpmConfig.description || packageJSON.description,
