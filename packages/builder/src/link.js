006119c3cdbdf2fcbd5bb8b2efc2da154fda6035
mathieudutour <mathieu@dutour.me>
2018-06-24 19:09:39 -0600

    move testDevMode to internal-utils

--- packages/builder/src/link.js
+++ packages/builder/src/link.js
@@ -7,52 +7,10 @@ import chalk from 'chalk'
 import semver from 'semver'
 import { get as getConfig } from '@skpm/internal-utils/tool-config'
 import getSkpmConfigFromPackageJSON from '@skpm/internal-utils/skpm-config'
-import { exec } from '@skpm/internal-utils/exec'
 import getSketchVersion from '@skpm/internal-utils/getSketchVersion'
+import checkDevMode from '@skpm/internal-utils/check-dev-mode'
 
 const { pluginDirectory } = getConfig()
-
-function testDevMode() {
-  const command = (action, value) =>
-    `/usr/bin/defaults ${action} ~/Library/Preferences/com.bohemiancoding.sketch3.plist AlwaysReloadScript ${value}`
-
-  return exec(command('read', ''), { encoding: 'utf8' })
-    .then(({ stdout }) => (stdout || '').trim() === '1')
-    .catch(() => false) // if reading fails, assume that it's not enabled
-    .then(enabled => {
-      if (!enabled) {
-        const yesno = require('yesno')
-        console.log(
-          `The Sketch developer mode is not enabled ${chalk.dim(
-            '(http://developer.sketchapp.com/introduction/preferences/#always-reload-scripts-before-running)'
-          )}.`
-        )
-        return new Promise((resolve, reject) => {
-          yesno.ask('Do you want to enable it? (y/N)', false, ok => {
-            if (ok) {
-              exec(command('write', '-bool YES'))
-                .then(() =>
-                  exec(
-                    '/usr/bin/defaults write com.bohemiancoding.sketch3 WebKitDeveloperExtras -bool true'
-                  )
-                )
-                .then(() =>
-                  console.log(
-                    `${chalk.green('success')} Sketch developer mode enabled`
-                  )
-                )
-                .then(resolve)
-                .catch(reject)
-            } else {
-              resolve()
-            }
-          })
-        })
-      }
-
-      return 'Already enabled'
-    })
-}
 
 const { argv } = yargs
   .help()
@@ -136,7 +94,7 @@ try {
     `${chalk.blue(skpmConfig.name)} - ${chalk.grey(skpmConfig.version)}`
   )
 
-  testDevMode()
+  checkDevMode()
     .then(() => getSketchVersion())
     .then(sketchVersion => {
       if (sketchVersion && semver.gte(sketchVersion, '45.0.0')) {
@@ -148,14 +106,6 @@ try {
         )
         console.log()
       }
-      process.exit(0)
-    })
-    .catch(err => {
-      console.log(
-        `${chalk.red('error')} Error while enabling the Sketch developer mode.`
-      )
-      console.log((err || {}).body || err)
-      console.log(`${chalk.blue('info')} Continuing...`)
       process.exit(0)
     })
 } catch (err) {
