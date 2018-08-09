d4f6b0ce1e6db855436c8dde4584d34202a6b7a7
mathieudutour <mathieu@dutour.me>
2018-08-09 07:13:20 -0600

    ask for the token

--- packages/skpm/src/commands/login.js
+++ packages/skpm/src/commands/login.js
@@ -1,4 +1,5 @@
 import ora from 'ora'
+import { prompt } from 'inquirer'
 import asyncCommand from '../utils/async-command'
 import auth from '../utils/auth'
 import github from '../utils/github'
@@ -10,6 +11,13 @@ export default asyncCommand({
     'Enter your GitHub access token and save it in the keychain.\n This token will be used to publish new releases on the repo.\nThe token needs the `repo` permissions in order to create releases. It will not be used for anything else.\n\nYou can create a new token here: https://github.com/settings/tokens/new',
 
   async handler(argv) {
+    if (!argv.token) {
+      const response = await prompt([
+        { name: 'token', message: 'GitHub Token', type: 'input' },
+      ])
+      Object.assign(argv, response)
+    }
+
     const spinner = ora({
       text: 'Checking if the token is valid',
       color: 'magenta',
