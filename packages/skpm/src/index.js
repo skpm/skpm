46874b5e1f91dd685484e41428d842b501d32df4
mathieudutour <mathieu@dutour.me>
2019-03-06 15:56:07 -0400

    Link to changelog in update message

--- packages/skpm/src/index.js
+++ packages/skpm/src/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import updateNotifier from 'update-notifier'
+import isCI from 'is-ci'
+import chalk from 'chalk'
 import yargs from 'yargs'
 import create from './commands/create'
 import log from './commands/log'
@@ -15,11 +17,38 @@ global.Promise = require('bluebird')
 
 checkVersion()
 
-updateNotifier({
+const notifier = updateNotifier({
   pkg,
-}).notify({
-  isGlobal: true,
 })
+
+if (notifier.update && notifier.update.latest !== pkg.version && !isCI) {
+  const old = notifier.update.current
+  const { latest } = notifier.update
+  let { type } = notifier.update
+  switch (type) {
+    case 'major':
+      type = chalk.red(type)
+      break
+    case 'minor':
+      type = chalk.yellow(type)
+      break
+    case 'patch':
+      type = chalk.green(type)
+      break
+    default:
+      break
+  }
+
+  const changelog = `https://skpm.io/release-notes/`
+  notifier.notify({
+    message:
+      `New ${type} version of ${pkg.name} available! ${chalk.red(
+        old
+      )} → ${chalk.green(latest)}\n` +
+      `${chalk.yellow('Changelog:')} ${chalk.cyan(changelog)}\n` +
+      `Run ${chalk.green(`npm install -g ${pkg.name}`)} to update!`,
+  })
+}
 
 yargs
   .scriptName('skpm')
